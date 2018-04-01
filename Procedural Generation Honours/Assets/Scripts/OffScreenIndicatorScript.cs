01fa7ee4722ae28565b67b1d6ff80aa2b85a2052
UOPNET\ahumphries1 <alix.humphries@students.plymouth.ac.uk>
2018-04-01 17:11:41 +0100

    Minimap Arrow
    Completed work on indicator arrow on minimap

--- Procedural Generation Honours/Assets/Scripts/OffScreenIndicatorScript.cs
+++ Procedural Generation Honours/Assets/Scripts/OffScreenIndicatorScript.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OffScreenIndicatorScript : MonoBehaviour {
-    public Transform endPoint;
+    Transform endPoint = null;
+    Transform getawayVehicle = null;
+
     private void Update()
     {
+        if(getawayVehicle == null)
+        {
+            if(GameObject.FindWithTag("GetawayVehicle") != null)
+            {
+                getawayVehicle = GameObject.FindWithTag("GetawayVehicle").transform;
+            }
+        }
+        if (endPoint == null)
+        {
+            if (GameObject.FindWithTag("EndPoint") != null)
+            {
+                endPoint = GameObject.FindWithTag("EndPoint").transform;
+            }
+        }
 
-        transform.LookAt(endPoint, Vector3.right);
+        if (getawayVehicle != null && endPoint != null)
+        {
+            Debug.DrawLine(getawayVehicle.position, endPoint.position, Color.red, 0.1f, false);
+            Vector3 dir = getawayVehicle.InverseTransformPoint(endPoint.position);
+            float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+
+            //angle += 180;
+
+            transform.localEulerAngles = new Vector3(0, 180, angle);
+        }
+        else
+        {
+            if(getawayVehicle != null && endPoint == null)
+            {
+                print("Endpoint is null");
+            }
+            if(getawayVehicle == null && endPoint != null)
+            {
+                print("Getaway is null");
+            }
+            if(getawayVehicle == null && endPoint == null)
+            {
+                print("both are null");
+            }
+        }
     }
 }
